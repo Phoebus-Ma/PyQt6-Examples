893c89879fd14cdac744bc91f2c610242517c04e
Phoebus <tianjindahai@126.com>
2024-08-15 10:23:37 +0800

    [PyQt] Add QFormLayout example.

---
+++ Class-1/09-FormLayout/MainWindow.py
@@ -0,0 +1,59 @@
+###
+# PyQt6 QFormLayout main window.
+#
+# License - MIT.
+###
+
+from PyQt6.QtWidgets import QWidget,       \
+    QFormLayout, QHBoxLayout, QVBoxLayout, \
+    QLabel, QLineEdit, QRadioButton, QPushButton
+
+
+class MainWindow(QWidget):
+# {
+    def __init__(self):
+    # {
+        super().__init__()
+
+        self.title = 'Form Layout'
+
+        self.initUI()
+    # }
+
+    def initUI(self):
+    # {
+        self.setWindowTitle(self.title)
+        self.setBaseSize(300, 200)
+
+        layout = self.createLayout()
+
+        self.setLayout(layout)
+    # }
+
+    def createLayout(self) -> QFormLayout:
+    # {
+        infoLayout = QFormLayout()
+
+        # Control 1.
+        infoLayout.addRow(QLabel('Name'), QLineEdit())
+
+        # Control 2.
+        sexLayout = QHBoxLayout()
+
+        sexLayout.addWidget(QRadioButton('Male'))
+        sexLayout.addWidget(QRadioButton('Female'))
+        infoLayout.addRow('Sex', sexLayout)
+
+        # Control 3.
+        addrLayout = QVBoxLayout()
+
+        addrLayout.addWidget(QLineEdit())
+        addrLayout.addWidget(QLineEdit())
+        infoLayout.addRow('Address', addrLayout)
+
+        # Control 4.
+        infoLayout.addRow(QPushButton('Yes'), QPushButton('No'))
+
+        return infoLayout
+    # }
+# }
---
+++ Class-1/09-FormLayout/README.md
@@ -0,0 +1,27 @@
+
+# Introduction
+
+PyQt6 QFormLayout example.
+
+
+# Screenshot
+
+![FormLayout](../res/09-FormLayout.png)
+
+
+# API
+
+English:
+
+- [QFormLayout](../../Class-0/QtWidget/QFormLayout.md)
+
+Chinese:
+
+- [QFormLayout](../../Class-0/QtWidget/QFormLayout.CN.md)
+
+
+# Usage
+
+```bash
+$ python main.py
+```
