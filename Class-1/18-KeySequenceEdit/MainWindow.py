5986700fe48b5d1a76b541d693e6cac04bb8b732
Phoebus <tianjindahai@126.com>
2025-05-05 00:06:07 +0800

    [pyqt] Add shortcuts key example.

---
+++ Class-1/18-KeySequenceEdit/MainWindow.py
@@ -0,0 +1,75 @@
+###
+# PyQt6 QKeySequenceEdit main window.
+#
+# License - MIT.
+###
+
+from PyQt6.QtCore import QDate, QTime, QDateTime
+from PyQt6.QtWidgets import QWidget, \
+    QVBoxLayout, QPushButton, QLabel, QKeySequenceEdit
+
+
+class MainWindow(QWidget):
+# {
+    def __init__(self):
+    # {
+        super().__init__()
+
+        self.title     = 'keySquenceEdit'
+        self.winWidth  = 640
+        self.winHeight = 320
+
+        self.initUI()
+    # }
+
+    def initUI(self):
+    # {
+        self.setWindowTitle(self.title)
+        self.setBaseSize(self.winWidth, self.winHeight)
+
+        self.label = QLabel("Please input ShortKey:")
+        self.keySquenceEdit = QKeySequenceEdit()
+
+        layout = self.createLayout()
+
+        self.setLayout(layout)
+    # }
+
+    def createLayout(self) -> QVBoxLayout:
+    # {
+        vLayout = QVBoxLayout()
+
+        # Label.
+        vLayout.addWidget(self.label)
+
+        # Key squence edit.
+        self.keySquenceEdit.setClearButtonEnabled(True)
+
+        vLayout.addWidget(self.keySquenceEdit)
+
+        # Get.
+        btnGet = QPushButton()
+        btnGet.setText('Get ShortKey')
+        btnGet.clicked.connect(self.btnGetClicked)
+
+        vLayout.addWidget(btnGet)
+
+        self.keySquenceEdit.keySequenceChanged.connect(self.update_label)
+
+        return vLayout
+    # }
+
+    # Real-time update label.
+    def update_label(self, key_sequence):
+    # {
+        self.label.setText(f'Current shortcuts key: {key_sequence.toString()}')
+    # }
+
+    # Display current key squence.
+    def btnGetClicked(self):
+    # {
+        sequence = self.keySquenceEdit.keySequence()
+        print(f'Current shortcuts key: {sequence.toString()}')
+        self.label.setText(f'Shortcut keys confirmed: {sequence.toString()}')
+    # }
+# }
---
+++ Class-1/18-KeySequenceEdit/README.md
@@ -0,0 +1,13 @@
+
+# Introduction
+
+Shortcuts key example.
+
+The examole use Qt QKeySquenceEdit.
+
+
+# Usage
+
+```bash
+$ python main.py
+```
