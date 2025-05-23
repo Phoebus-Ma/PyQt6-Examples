8c88e92ac800b5ef80d513a0a8830ee1d74aacb8
Phoebus <tianjindahai@126.com>
2025-05-23 14:27:15 +0800

    [ScrollArea] Add vertical scroll area example.

---
+++ Class-1/VerScrollArea/MainWindow.py
@@ -0,0 +1,57 @@
+###
+# PyQt6 VerticalScrollArea main window.
+#
+# License - MIT.
+###
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QWidget,
+    QVBoxLayout,
+    QLabel,
+    QScrollArea
+)
+
+class MainWindow(QWidget):
+# {
+    def __init__(self):
+    # {
+        super().__init__()
+
+        self.title     = 'VerticalScrollArea'
+        self.winWidth  = 320
+        self.winHeight = 240
+
+        self.initUI()
+    # }
+
+    def initUI(self):
+    # {
+        self.setWindowTitle(self.title)
+        self.setMinimumSize(self.winWidth, self.winHeight)
+
+        # Create scroll area container.
+        scrollArea = QScrollArea(self)
+
+        # Allow content adaptation.
+        scrollArea.setWidgetResizable(True)
+
+        # Auto display vertical scroll bar.
+        scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+
+        # Create content control.
+        contentWidget = QWidget()
+        contentLayout = QVBoxLayout(contentWidget)
+
+        # Add a lot of content.
+        for i in range(50):
+            contentLayout.addWidget(QLabel(f"Item {i + 1}", self))
+
+        # Place content in scroll area.
+        scrollArea.setWidget(contentWidget)
+
+        mainLayout = QVBoxLayout(self)
+        mainLayout.addWidget(scrollArea)
+        self.setLayout(mainLayout)
+    # }
+# }
